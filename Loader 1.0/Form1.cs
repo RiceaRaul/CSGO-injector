878e5c928828935d013a515f26041a2aa331d705
Raul202 <55659437+RiceaRaul@users.noreply.github.com>
2022-05-28 22:28:19 +0300

    Update Form1.cs

--- Loader 1.0/Form1.cs
+++ Loader 1.0/Form1.cs
@@ -61,7 +61,7 @@ namespace Loader_1._0
             string password = ePassword.Text;
             string HWID = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
             int expire = 0 ;
-            MySqlConnection connection = new MySqlConnection("Server=188.212.101.56; Database=atriosxy_loader;User ID=atriosxy_loader;Password=tk#Ieyh}8eIq; Pooling=true;");
+            MySqlConnection connection = new MySqlConnection("Server=localhost; Database=atriosxy_loader;User ID=atriosxy_loader;Password=tk#Ieyh}8eIq; Pooling=true;");
             connection.Open();
             if (connection.State == ConnectionState.Open)
             {
